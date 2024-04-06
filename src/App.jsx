86eae31794e7af347bdb64135f2aa9410214e884
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-04-06 20:03:29 +0530

    Added Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
 import { Routes } from "react-router-dom";
+
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Routes></Routes>
+      {/* <Routes></Routes> */}
+      <Footer />
     </>
   );
 }
